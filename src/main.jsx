9876c97a5c7f94d18f3721f6c4d54485eeb63a19
Arthur Baduyen <arthur.baduyen@chromedia.com>
2026-03-27 23:37:32 +0800

    Serve portfolio fonts from public assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import '@fontsource/sora/latin-400.css';
-import '@fontsource/sora/latin-500.css';
-import '@fontsource/sora/latin-600.css';
-import '@fontsource/sora/latin-700.css';
-import '@fontsource/space-grotesk/latin-500.css';
-import '@fontsource/space-grotesk/latin-700.css';
 import App from './App';
 import './styles.css';
 
